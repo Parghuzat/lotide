5afb215ede45c2afcc22af3decb02656b71faa2d
Parghuzat <paguzati@hotmail.com>
2021-10-26 05:27:54 +0000

    Checking value of 1st element

--- head.js
+++ head.js
@@ -16,3 +16,4 @@ assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5,6,7]), 1);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "gello");
+
