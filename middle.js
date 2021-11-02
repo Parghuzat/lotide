efba9f003e749ab3353fd3c39274b56d43796d08
Parghuzat <paguzati@hotmail.com>
2021-11-01 20:23:34 -0700

    middle

--- middle.js
+++ middle.js
@@ -1,3 +1,6 @@
+const assertArraysEqual = require('assertArraysEqual')
+
+
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -9,11 +12,5 @@ const middle = function(array) {
   return [array[Math.floor((array.length/2))]]
 }
   
+module.exports = middle; 
 
-
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2]))
-console.log(middle([1]))
