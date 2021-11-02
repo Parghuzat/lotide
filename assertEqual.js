5e2ca5cbb9be2716740aa92595e4569a91d91b47
Parghuzat <90359579+Parghuzat@users.noreply.github.com>
2021-11-01 17:36:21 -0700

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log("Assertion Passed: " + actual + " === " + expected);
+    return console.log(`Assertion Passed:  ${actual}  ===   ${expected}`);
   }
   else if (actual !== expected) {
-    return console.log("Assertion Failed: " + actual + " !== " + expected);
+    return console.log(`Assertion Failed: ${actual} !==  ${expected}`);
   }
 }
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(3, 3);
+
+module.exports = assertEqual;
